82a0c469626ded86a577b18345deedc3efa13bbf
Renato Bezerra <bezerra.renato@gmail.com>
2024-11-24 23:57:10 -0300

    Added home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,15 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg';
+import LinkButton from '../../components/layout/LinkButton/LinkButton';
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <section className={styles.homeContainer}>
+          <h1>Bem-vindo ao <span>Costs</span></h1>
+          <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+          <LinkButton to="/project/new" text="Criar Projeto" />
+          <img src={savings} alt="Costs Image" aria-hidden="true" />
+        </section>
     );
 }
 
