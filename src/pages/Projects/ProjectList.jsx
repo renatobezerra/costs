77829dada0dddf7fd2d2d8ecb829737e4b149bfb
Renato Bezerra <bezerra.renato@gmail.com>
2024-11-26 22:49:32 -0300

    Initi Project list

--- src/pages/Projects/ProjectList.jsx
+++ src/pages/Projects/ProjectList.jsx
@@ -1,17 +1,22 @@
+import Container from '../../components/layout/Container/Container';
 import Message from '../../components/layout/Message/Message';
+import LinkButton from '../../components/form/LinkButton';
 import { useLocation } from 'react-router-dom';
+import styles from './ProjectList.module.css';
 
 function ProjectList() {
-
   const location = useLocation();
-  let message = location?.state?.message ?? 'nha';
 
   return (
-    <div>
-      <h1>Projects</h1>
-      {
-        message && <Message type='success' message={message}/>
-      }
+    <div className={styles.projectContainer}>
+      <div className={styles.titleContainer}>
+        <Message type='success' message={location?.state?.message}/>
+        <h1>Projects</h1>
+        <LinkButton to="/project/new" text="Criar Projeto" />
+      </div>
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
